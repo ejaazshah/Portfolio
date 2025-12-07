dc01e412fbd5b81ffafc46c45f41512017073090
ejaazshah <shahejaaz451@gmail.com>
2025-12-07 21:21:31 +0430

    working on nav home page

---
+++ src/components/nav.jsx
@@ -0,0 +1,19 @@
+import './nav.css'
+import logo from '../../public/logo/EjaazShahlogo/vector/default-monochrome-black.svg'
+function nav(){
+  return (
+    <header className='nav'>
+     <img class="logo" src={logo} alt=""></img>
+   <nav className=''>
+      <ul>
+        <a  href="#">Home</a>
+        <a  href="#">About</a>
+        <a href="#">Projects</a>
+        <a href="#">Contact</a>
+      </ul>
+    </nav>
+    </header>
+  )
+}
+
+export default nav;
